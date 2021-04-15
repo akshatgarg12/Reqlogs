083af8f4c3ea5c9466d8554fadd043bee1a2c849
Akshat Garg <akshatarungarg78@gmail.com>
2021-04-15 14:20:36 +0530

    migrated to typescript

--- idea.md
+++ idea.md
@@ -1,2 +1,5 @@
 # Create a EJS template to show the properties of request
 # use a logger to log the details of request in the terminal
+# find an alternative of table-printer
+# save requests in local storage for persistence
+# use ts
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-const express = require('express')
+import express , {Request, Response} from 'express'
+import path from "path";
 const app = express()
-const cors = require('cors')
+import cors from 'cors'
 const PORT = process.env.PORT || 8080
-// const {logs, API_LOGGER} = require('./middleware/request')
-const {RequestLogger} = require('./middleware/request')
-
+import {RequestLogger} from './middleware/request'
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'src'+ '/public'))
+app.set( "views", path.join( __dirname, "views" ) );
 app.set('view engine','ejs');
 
 // logs the request data to console in table form
@@ -19,13 +19,13 @@ const RL = new RequestLogger({ignore_urls : ['/logs'],parameters:["index","path"
 app.use(RL.Console())
 app.use('/logs',RL.Webpage())
 
-app.get('/' , (req, res) => {
+app.get('/' , (_req:Request, res:Response) : void => {
   res.send("hello world")
 })
-app.post('/' , (req, res) => {
+app.post('/' , (_req:Request, res:Response) : void  => {
   res.send("hello world")
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () : void => {
   console.log(`Server started at port : ${PORT}`)
 })
